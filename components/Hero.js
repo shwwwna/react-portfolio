70d154e6ed4a22a81b14a2074ffa076bf0d8dc5c
shwwwna <107602771+shwwwna@users.noreply.github.com>
2023-01-16 21:19:40 +0800

    update availability

--- components/Hero.js
+++ components/Hero.js
@@ -42,7 +42,7 @@ const Hero = () => {
 					</svg>
 					<span class="sr-only">Info</span>
 					<div class="ml-3 text-sm font-medium mix-blend-normal text-gray-300">
-						Currently, I am booked until February 2023.
+						Currently, I am looking for part-time work.
 					</div>
 				</div>
 			</div>
@@ -51,18 +51,18 @@ const Hero = () => {
 					type="button"
 					target="_blank"
 					rel="noreferrer"
-					href="https://drive.google.com/file/d/1_py2XW-1qUlOKZjQRU1-i_cf9TJuTJFC/view?usp=share_link"
+					href="https://www.linkedin.com/in/sheena-tuazon/"
 					className="border border-slate-900 text-white bg-slate-900 hover:bg-slate-800 focus:ring-4 focus:ring-slate-300 font-medium rounded-lg text-sm px-5 py-2.5 md:mr-2 mb-2  focus:outline-none transition-all duration-200 shadow w-full md:w-fit text-center ">
-					Download my resume
+					Connect in LinkedIn
 				</a>
 
 				<a
 					type="button"
 					target="_blank"
 					rel="noreferrer"
-					href="https://www.linkedin.com/in/sheena-tuazon/"
+					href="https://drive.google.com/file/d/1_py2XW-1qUlOKZjQRU1-i_cf9TJuTJFC/view?usp=share_link"
 					className=" border border-gray-300 focus:outline-none hover:bg-teal-700 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 md:mr-2 mb-2  transition-all duration-200 shadow w-full md:w-fit text-center ">
-					Contact me
+					Download resume
 				</a>
 			</div>
 		</div>
